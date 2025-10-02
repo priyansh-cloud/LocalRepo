0a6cb2320c96019724579aa119a807183256e087
priyanshu-cloud <priyanshu.mahto7781>
2025-10-02 14:17:33 +0530

    Added count digit sol

---
+++ DigitsProblem/CountDigits.java
@@ -0,0 +1,21 @@
+
+
+
+public class CountDigits {
+    public static void main(String[] args) {
+
+        int n= 9867;
+        int count =0;
+
+        while(n>0){
+            
+            int ld= n%10;
+            count ++;
+            n = n/10;
+
+        }
+        System.out.println(count);
+        
+    }
+    
+}
