edb4e929f54b86d5612792854186f6d43f40b51e
priyanshu-cloud <priyanshu.mahto7781>
2025-10-12 00:00:54 +0530

    Hollow_Triangle.java

---
+++ PatternsProblem/Hollow_Triangle.java
@@ -0,0 +1,20 @@
+public class Hollow_Triangle {
+    public static void main(String[] args) {
+        int n =5;
+for(int i=1; i<=n; i++){
+    // space
+    for(int j=1; j<=n-i; j++){
+        System.out.print(" ");
+    }
+    //star
+    for(int j=1; j<=2*i-1; j++){
+        if(i==n || j==1 ||  j==2*i-1){
+
+            System.out.print("*" );
+        }else System.out.print(" ");
+        
+    }
+    System.out.println();
+}
+    }
+}
