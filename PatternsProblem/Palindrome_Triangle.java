b7f08937f3f6e322762f3fda438a78428e509efb
priyanshu-cloud <priyanshu.mahto7781>
2025-10-16 22:17:18 +0530

    Palindrome Triangle Added

---
+++ PatternsProblem/Palindrome_Triangle.java
@@ -0,0 +1,39 @@
+public class Palindrome_Triangle {
+    public static void main(String[] args) {
+        
+        int n = 3;
+        for(int i=1; i<=n; i++){
+
+            // Space
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            // Star
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            for(int j=2; j<=i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+         
+         for(int i=n-1; i>=1; i--){
+
+            // Space
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            // Star
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            for(int j=2; j<=i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+         
+
+    }
+}
