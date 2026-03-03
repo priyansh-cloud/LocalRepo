9fa54cf5c4c76383830864c1dabc33dd44410f3e
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2026-03-03 22:37:04 +0530

    palindrome by using recursion

---
+++ BasicRecursion.java/palindrome_recursion.java
@@ -0,0 +1,33 @@
+
+
+public class palindrome_recursion{
+
+
+    public static boolean f(int i, int n, int arr[]){
+
+        if(i>=n/2){
+            return true;
+        }
+
+        if(arr[i] != arr[n-i-1]){
+            return false;
+        }
+        int temp= arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1]= temp;
+
+        return f(i+1,n,arr);
+
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {2,1,1,2};
+        int n = arr.length;
+        boolean ans = f(0,n,arr);
+
+        System.out.println(ans);
+        
+    }
+}
+
+// note : replace int arr[] --> String s for string values
